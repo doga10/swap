f42363739c6b7199392430fea55620c12531439c
Douglas Dennys <douglasdennys@yahoo.com>
2020-05-10 16:10:52 -0300

    adicionando config no sysctl.conf

--- main.sh
+++ main.sh
@@ -2,3 +2,6 @@
 
 echo "Create Swap - 4GB"
 swapon --show && free -h && df -h && fallocate -l 4G /swapfile && ls -lh /swapfile && chmod 600 /swapfile && ls -lh /swapfile && mkswap /swapfile && swapon /swapfile && swapon --show && free -h && cp /etc/fstab /etc/fstab.bak && echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab 
+
+echo "Move /etc/sysctl.conf"
+mv ./lib/sysctl.conf /etc/sysctl.conf
